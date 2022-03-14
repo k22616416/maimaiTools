a9b531788c32fa9005d0254b2a25fd37ac0576ba
DESKTOP-U4AGQO0\kk013 <kk013579@gmail.com>
2022-03-14 11:17:18 +0800

    新增抓圖片功能(未完成)

--- maimai_Info.py
+++ maimai_Info.py
@@ -9,11 +9,15 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as Soup
 from selenium.webdriver.common.action_chains import ActionChains
 from pandas.core.frame import DataFrame
+from enum import Enum
+from PIL import Image
+import requests
 
 username = 'k22616416'
 password = 'kk013579'
 
 url = 'https://lng-tgk-aime-gw.am-all.net/common_auth/login?site_id=maimaidxex&redirect_url=https://maimaidx-eng.com/maimai-mobile/&back_url=https://maimai.sega.com/'
+
 
 options = Options()
 options.add_argument("--disable-notifications")
@@ -22,27 +26,66 @@ options.add_experimental_option(
 
 chrome = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
 chrome.get(url)
-WebDriverWait(chrome, 20).until(EC.presence_of_element_located(
-    (By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dt/ul/li/span')))
-chrome.find_element_by_xpath(
-    '/html/body/div[2]/div[2]/main/article/section[2]/dl/dt/ul/li/span').click()
 
-WebDriverWait(chrome, 20).until(
-    EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[1]/div[2]/input')))
-# usernameObj = chrome.find_element_by_id('sid')
-usernameObj = chrome.find_element_by_xpath(
-    '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[1]/div[2]/input')
-print(usernameObj)
 
-usernameObj.send_keys(username)
-WebDriverWait(chrome, 20).until(
-    EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[2]/div[2]/input')))
-passwordObj = chrome.find_element_by_xpath(
-    '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[2]/div[2]/input')
-passwordObj.send_keys(password)
+def Login_maimai_net():
 
-WebDriverWait(chrome, 20).until(
-    EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/div/input')))
+    # click SEGA ID button
+    WebDriverWait(chrome, 20).until(EC.presence_of_element_located(
+        (By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dt/ul/li/span')))
+    chrome.find_element_by_xpath(
+        '/html/body/div[2]/div[2]/main/article/section[2]/dl/dt/ul/li/span').click()
 
-chrome.find_element_by_xpath(
-    '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/div/input').click()
+    # insert username and password
+    WebDriverWait(chrome, 20).until(
+        EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[1]/div[2]/input')))
+    chrome.find_element_by_xpath(
+        '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[1]/div[2]/input').send_keys(username)
+    WebDriverWait(chrome, 20).until(
+        EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[2]/div[2]/input')))
+    chrome.find_element_by_xpath(
+        '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[2]/div[2]/input').send_keys(password)
+    WebDriverWait(chrome, 20).until(
+        EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/div/input')))
+
+    # click login button
+    chrome.find_element_by_xpath(
+        '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/div/input').click()
+
+
+def GetNewPhotos():
+
+    chrome.get("https://maimaidx-eng.com/maimai-mobile/photo/")
+    WebDriverWait(chrome, 20).until(
+        EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]')))
+    photoFrames = chrome.find_elements_by_xpath('/html/body/div[2]/div[2]')
+    print(len(photoFrames))
+    for frame in photoFrames:
+        photo = frame.find_element_by_xpath(
+            '/html/body/div[2]/div[2]/div/img[3]')
+
+        imgurl = photo.get_attribute("src")
+        img_data = requests.get(imgurl).content
+        with open('./image_name.jpg', 'wb') as handler:
+            handler.write(img_data)
+
+
+def main():
+    Login_maimai_net()
+    GetNewPhotos()
+
+
+if __name__ == '__main__':
+    main()
+
+
+class WorkStatus(Enum):
+    Home = 1
+    PlayData = 2
+    Friends = 3
+    Photos = 4
+    Records = 5
+    Event = 6
+    Collection = 7
+    Ranking = 8
+    Settings = 9
