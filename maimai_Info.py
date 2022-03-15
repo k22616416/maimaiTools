08e7e6d09c716a5c7f831660f969176e3400abed
陳 廷偉 <kk013579@gmail.com>
2022-03-16 02:14:16 +0800

    自動抓圖OK 等級判定還沒好

--- maimai_Info.py
+++ maimai_Info.py
@@ -1,5 +1,7 @@
 from queue import Empty
 from urllib import request
+from matplotlib import image
+from numpy import array, histogram
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -17,12 +19,46 @@ import requests
 from io import StringIO
 import os
 import urllib.request
-
+import math
+import operator
+from functools import reduce
 
 username = 'k22616416'
 password = 'kk013579'
 
 url = 'https://lng-tgk-aime-gw.am-all.net/common_auth/login?site_id=maimaidxex&redirect_url=https://maimaidx-eng.com/maimai-mobile/&back_url=https://maimai.sega.com/'
+imageSavePath = 'files'
+
+MUSIC_LEVEL_IMAGE_PATHS = {
+    "BASIC": "/asset/level/diff_basic.png",
+    "ADVANCED": "\\asset\\level\\diff_advanced.png",
+    "EXPERT": "\\asset\\level\\diff_expert.png",
+    "MASTER": "\\asset\\level\\diff_master.png"
+}
+
+MUSIC_TYPE_IMAGE_PATHS = {
+    "DX": "\\asset\\type\\music_dx.png",
+    "STANDARD": "\\asset\\type\\music_standard.png"
+}
+
+
+# class MusicLevels(Enum):
+#     BASIC = 0
+#     ADCANCED = 1
+#     EXPERT = 2
+#     MASTER = 3
+
+
+class WorkStatus(Enum):
+    Home = 1
+    PlayData = 2
+    Friends = 3
+    Photos = 4
+    Records = 5
+    Event = 6
+    Collection = 7
+    Ranking = 8
+    Settings = 9
 
 
 options = Options()
@@ -59,18 +95,30 @@ def Login_maimai_net():
         '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/div/input').click()
 
 
+def CompareImages(pic1: image, pic2: image) -> bool:
+    histogram1 = pic1.histogram()
+    histogram2 = pic2.histogram()
+    differ = math.sqrt(reduce(operator.add, list(
+        map(lambda a, b: (a-b)**2, histogram1, histogram2)))/len(histogram1))
+    return (differ == 0)
+
+
+def LevelClassification(img1) -> str:
+    for i in MUSIC_LEVEL_IMAGE_PATHS:
+        if CompareImages(Image.open(os.getcwd() + MUSIC_LEVEL_IMAGE_PATHS[i]), Image.open(img1)):
+            return i
+
+
 def GetNewPhotos():
     chrome.get("https://maimaidx-eng.com/maimai-mobile/photo/")
     WebDriverWait(chrome, 20).until(
         EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '.m_10.p_5.f_0')))
-    # photoFrames = chrome.find_elements_by_xpath('/html/body/div[2]/div[2]')
     photoFrames = chrome.find_elements_by_css_selector('.m_10.p_5.f_0')
 
     print(len(photoFrames))
     count = 0
+    mainWindow = chrome.current_window_handle
     for frame in photoFrames:
-        # photo = frame.find_element_by_xpath(
-        #     '/html/body/div[2]/div[2]/div/img[3]')
         WebDriverWait(chrome, 20).until(
             EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '.block_info.p_3.f_11.white')))
         dateTag = frame.find_element_by_css_selector(
@@ -80,33 +128,48 @@ def GetNewPhotos():
             print('Date empty')
             continue
 
-        # 3/14 photo css需要修改
-        photo = frame.find_element_by_css_selector('.w_430')
-        imgurl = photo.get_attribute("src")
-        chrome.get(imgurl)
+        photo = frame.find_elements_by_css_selector('.w_430')[1]
 
-        with open(date+'.png', 'wb') as file:
-            file.write(chrome.find_element_by_xpath(
-                "/html/body/img").screenshot_as_png)
+        imgurl = photo.get_attribute("src")
+        chrome.execute_script("window.open('"+imgurl+"');")
+        chrome.switch_to_window(
+            chrome.window_handles[len(chrome.window_handles)-1])
+
+        dirPath = os.path.join(imageSavePath, (date.split(' '))[
+            0].replace('/', ''))
+
+        # 檢查資料夾路徑
+        if not os.path.exists(dirPath):
+            os.makedirs(dirPath)
+            print("Make new dir : " + dirPath)
+
+        # 去掉日期中的/跟:
+        # level = LevelClassification()
+        imgName = os.path.join(
+            dirPath, (date.replace('/', '')).replace(':', '.') + '.png')
+
+        # 檢查是否已存在
+        if not os.path.isfile(imgName):
+            # 不存在就存檔
+            WebDriverWait(chrome, 20).until(
+                EC.visibility_of_all_elements_located((By.XPATH, "/html/body/img")))
+            with open(imgName, 'wb') as file:
+                file.write(chrome.find_element_by_xpath(
+                    "/html/body/img").screenshot_as_png)
+            print("Save : " + imgName)
+
+        chrome.close()
+        chrome.switch_to_window(mainWindow)
         count += 1
 
 
 def main():
-    Login_maimai_net()
-    GetNewPhotos()
+    print(LevelClassification(
+        "https://maimaidx-eng.com/maimai-mobile/img/diff_advanced.png"))
+
+    # Login_maimai_net()
+    # GetNewPhotos()
 
 
 if __name__ == '__main__':
     main()
-
-
-class WorkStatus(Enum):
-    Home = 1
-    PlayData = 2
-    Friends = 3
-    Photos = 4
-    Records = 5
-    Event = 6
-    Collection = 7
-    Ranking = 8
-    Settings = 9
---
+++ t.py
@@ -0,0 +1,4 @@
+import os
+
+
+print(os.path.join('files', '123'))
