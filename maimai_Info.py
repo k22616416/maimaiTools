239c3020bdf8bf49036cbe79e9d0b3218c07ea1f
陳 廷偉 <kk013579@gmail.com>
2022-04-12 21:11:26 +0800

    4/12 修改為Linux可執行的版本

--- maimai_Info.py
+++ maimai_Info.py
@@ -1,10 +1,12 @@
+from ast import Global
+import asyncio
 from http import client
 from multiprocessing.connection import wait
 
 
 import discord
 from discord.ext import tasks, commands
-
+import logging
 from queue import Empty
 from urllib import request
 from matplotlib import image
@@ -26,6 +28,7 @@ from PIL import Image
 import requests
 # from io import StringIO
 import os
+import sys
 # import urllib.request
 import math
 import operator
@@ -35,8 +38,11 @@ import json
 import time
 import queue
 import datetime
+from io import BytesIO
 
 global userCollection
+global PhotoTaskBusyFlag
+global chromeBusyCount
 
 # usernameCollection = ['k22616416']
 # passwordCollection = ['kk013579']
@@ -49,16 +55,17 @@ imageSavePath = 'files'
 DISCORD_BOT_TOKEN = ''
 
 MUSIC_LEVEL_IMAGE_PATHS = {
-    "BASIC": "\\asset\\level\\diff_basic.png",
-    "ADVANCED": "\\asset\\level\\diff_advanced.png",
-    "EXPERT": "\\asset\\level\\diff_expert.png",
-    "MASTER": "\\asset\\level\\diff_master.png",
-    "REMASTER": "\\asset\\level\\diff_remaster.png"
+    "BASIC": "/asset/level/diff_basic.png",
+    "ADVANCED": "/asset/level/diff_advanced.png",
+    "EXPERT": "/asset/level/diff_expert.png",
+    "MASTER": "/asset/level/diff_master.png",
+    "REMASTER": "/asset/level/diff_remaster.png"
 }
 
 MUSIC_TYPE_IMAGE_PATHS = {
-    "DX": "\\asset\\type\\music_dx.png",
-    "でらっくす": "\\asset\\type\\music_standard.png"
+    "DX": "/asset/type/music_dx.png",
+    "でらっくす": "/asset/type/music_standard.png"
+
 }
 
 SAVE_JSON_PATHS = '/files/AutoSaveList.json'
@@ -90,51 +97,6 @@ options.add_experimental_option(
     "excludeSwitches", ['enable-automation', 'enable-logging'])
 
 
-# class MyCog(commands.Cog):
-#     def __init__(self, bot):
-#         self.index = 0
-#         self.bot = bot
-#         self.printer.start()
-
-#     def cog_unload(self):
-#         self.printer.cancel()
-
-#     @tasks.loop(seconds=5.0)
-#     async def printer(self):
-#         print(self.index)
-#         self.index += 1
-
-#     @printer.before_loop
-#     async def before_printer(self):
-#         print('waiting...')
-#         await self.bot.wait_until_ready()
-
-
-def Login_maimai_net(chrome: webdriver, username: str, password: str):
-
-    # click SEGA ID button
-    WebDriverWait(chrome, 20).until(EC.presence_of_element_located(
-        (By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dt/ul/li/span')))
-    chrome.find_element_by_xpath(
-        '/html/body/div[2]/div[2]/main/article/section[2]/dl/dt/ul/li/span').click()
-
-    # insert username and password
-    WebDriverWait(chrome, 20).until(
-        EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[1]/div[2]/input')))
-    chrome.find_element_by_xpath(
-        '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[1]/div[2]/input').send_keys(username)
-    WebDriverWait(chrome, 20).until(
-        EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[2]/div[2]/input')))
-    chrome.find_element_by_xpath(
-        '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[2]/div[2]/input').send_keys(password)
-    WebDriverWait(chrome, 20).until(
-        EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/div/input')))
-
-    # click login button
-    chrome.find_element_by_xpath(
-        '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/div/input').click()
-
-
 def CompareImages(pic1: image, pic2: image) -> bool:
     histogram1 = pic1.histogram()
     histogram2 = pic2.histogram()
@@ -145,7 +107,7 @@ def CompareImages(pic1: image, pic2: image) -> bool:
 
 def LevelClassification(imgUrl) -> str:
     for i in MUSIC_LEVEL_IMAGE_PATHS:
-        if CompareImages(Image.open(os.getcwd() + MUSIC_LEVEL_IMAGE_PATHS[i]), Image.open(requests.get(imgUrl, stream=True).raw)):
+        if CompareImages(Image.open(os.getcwd() + MUSIC_LEVEL_IMAGE_PATHS[i]), Image.open(BytesIO(requests.get(imgUrl).content))):
             return i
 
 
@@ -162,10 +124,12 @@ def GetNewPhotos(chrome: webdriver, username: str):
         WebDriverWait(chrome, 20).until(
             EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '.m_10.p_5.f_0')))
     except exceptions.TimeoutException as exception:
-        print('登入失敗，伺服器維修或帳號密碼有誤')
+        print(time.strftime("%Y\\%m\\%d %H:%M:%S",
+              time.localtime()), '登入失敗，伺服器維修或帳號密碼有誤')
         return []
 
-    photoFrames = chrome.find_elements_by_css_selector('.m_10.p_5.f_0')
+    photoFrames = chrome.find_elements(
+        by=By.CSS_SELECTOR, value='.m_10.p_5.f_0')
 
     mainWindow = chrome.current_window_handle
     NewPhotosCollection = []
@@ -174,30 +138,33 @@ def GetNewPhotos(chrome: webdriver, username: str):
             WebDriverWait(chrome, 20).until(
                 EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '.block_info.p_3.f_11.white')))
         except exceptions.TimeoutException as exception:
+            print(time.strftime("%Y\\%m\\%d %H:%M:%S",
+                                time.localtime()), exception.msg)
             continue
-        dateTag = frame.find_element_by_css_selector(
-            '.block_info.p_3.f_11.white')
+        dateTag = frame.find_element(
+            by=By.CSS_SELECTOR, value='.block_info.p_3.f_11.white')
         date = dateTag.text
         if date == None:
-            print('Date empty')
+            print(time.strftime("%Y/%m/%d %H:%M:%S",
+                  time.localtime()), 'Date empty')
             continue
 
         # 抓歌名
-        songName = frame.find_element_by_css_selector(
-            '.black_block.w_430.m_3.m_b_5.p_5.t_l.f_15.break').text
+        songName = frame.find_element(
+            by=By.CSS_SELECTOR, value='.black_block.w_430.m_3.m_b_5.p_5.t_l.f_15.break').text
 
         # 抓圖片Url
-        photoTag = frame.find_elements_by_css_selector('.w_430')[1]
+        photoTag = frame.find_elements(by=By.CSS_SELECTOR, value='.w_430')[1]
         imgurl = photoTag.get_attribute("src")
 
         # 抓難度
-        levelUrl = frame.find_element_by_css_selector(
-            '.h_16.f_l').get_attribute("src")
+        levelUrl = frame.find_element(
+            by=By.CSS_SELECTOR, value='.h_16.f_l').get_attribute("src")
         level = LevelClassification(levelUrl)
 
         # 抓類型
-        musicTypeUrl = frame.find_element_by_css_selector(
-            '.music_kind_icon.f_r').get_attribute("src")
+        musicTypeUrl = frame.find_element(
+            by=By.CSS_SELECTOR, value='.music_kind_icon.f_r').get_attribute("src")
         musicType = MusicTypefication(musicTypeUrl)
 
         # 開新分頁存圖
@@ -211,7 +178,8 @@ def GetNewPhotos(chrome: webdriver, username: str):
         # 檢查資料夾路徑
         if not os.path.exists(dirPath):
             os.makedirs(dirPath)
-            print("Make new dir : " + dirPath)
+            print(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()),
+                  "Make new dir : " + dirPath)
 
         # 去掉日期中的/跟:
         imgName = os.path.join(
@@ -224,11 +192,14 @@ def GetNewPhotos(chrome: webdriver, username: str):
                 WebDriverWait(chrome, 20).until(
                     EC.visibility_of_all_elements_located((By.XPATH, "/html/body/img")))
             except exceptions.TimeoutException as exception:
+                print(time.strftime("%Y\\%m\\%d %H:%M:%S",
+                                    time.localtime()), exception.msg)
                 continue
             with open(imgName, 'wb') as file:
-                file.write(chrome.find_element_by_xpath(
-                    "/html/body/img").screenshot_as_png)
-            print("Save : " + imgName)
+                file.write(chrome.find_element(
+                    by=By.XPATH, value="/html/body/img").screenshot_as_png)
+            print(time.strftime("%Y/%m/%d %H:%M:%S",
+                  time.localtime()), "Save : " + imgName)
             NewPhotosCollection.append(imgName)
 
         chrome.close()
@@ -273,34 +244,158 @@ def SetAutoSaveUser(author: str, username: str, passwd: str):
     return 0
 
 
-def GetUserList():
-    global userCollection
-    fileName = os.path.join(os.getcwd() + SAVE_JSON_PATHS)
-    file = open(fileName, "r+")
-    try:
-        with file as f:
-            userCollection = json.load(f)
-    except:
-        return 1
-
-    file.close()
-    return 0
-
-
 client = discord.Client()
 # 調用event函式庫
+
+
+class PhotosTask:
+
+    global PhotoTaskBusyFlag
+    PhotoTaskBusyFlag = False
+
+    def __init__(self):
+        global chromeBusyCount
+        chromeBusyCount = 0
+        self.GetNewPhotosTask.start()
+
+    def GetUserList(self):
+        global userCollection
+        fileName = os.path.join(os.getcwd() + SAVE_JSON_PATHS)
+        file = open(fileName, "r+")
+        try:
+            with file as f:
+                userCollection = json.load(f)
+        except:
+            return 1
+        file.close()
+        return 0
+
+    def __del__(self):
+        self.GetNewPhotosTask.stop()
+        global PhotoTaskBusyFlag
+        PhotoTaskBusyFlag = False
+
+    def Login_maimai_net(self, chrome: webdriver, username: str, password: str):
+        # click SEGA ID button
+        WebDriverWait(chrome, 20).until(EC.presence_of_element_located(
+            (By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dt/ul/li/span')))
+        chrome.find_element(
+            by=By.XPATH, value='/html/body/div[2]/div[2]/main/article/section[2]/dl/dt/ul/li/span').click()
+        # insert username and password
+        WebDriverWait(chrome, 20).until(
+            EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[1]/div[2]/input')))
+        chrome.find_element(
+            by=By.XPATH, value='/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[1]/div[2]/input').send_keys(username)
+        WebDriverWait(chrome, 20).until(
+            EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[2]/div[2]/input')))
+        chrome.find_element(
+            by=By.XPATH, value='/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/ul/li[2]/div[2]/input').send_keys(password)
+        WebDriverWait(chrome, 20).until(
+            EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/div/input')))
+        # click login button
+        chrome.find_element(
+            by=By.XPATH, value='/html/body/div[2]/div[2]/main/article/section[2]/dl/dd/form/div/input').click()
+
+    @tasks.loop(minutes=10)
+    async def GetNewPhotosTask(self):
+        global PhotoTaskBusyFlag
+        global chromeBusyCount
+        if datetime.datetime.now().time() > NOT_IN_SERVICE_START_TIME.time() and datetime.datetime.now().time() < NOT_IN_SERVICE_END_TIME.time():
+            print(time.strftime("%Y\\%m\\%d %H:%M:%S",
+                                time.localtime()), 'maimai net維修')
+            return
+        if PhotoTaskBusyFlag:
+            if chromeBusyCount > 5:
+                print(time.strftime("%Y\\%m\\%d %H:%M:%S",
+                                    time.localtime()), '程式錯誤 重開中')
+                os.system('python3 maimai_Info.py')
+                os._exit(0)
+
+            print('Chrome in busy')
+            chromeBusyCount += 1
+
+            return
+        if self.GetUserList() != 0:
+            print(time.strftime("%Y\\%m\\%d %H:%M:%S",
+                                time.localtime()), "GetUserList error")
+            return
+        for users in userCollection:
+            user_tmp = users.get('valuse').get('username')
+            pawd_tmp = users.get('valuse').get('password')
+            userDcId = users.get('discordID')
+            PhotoTaskBusyFlag = True
+            try:
+                chrome = webdriver.Chrome(options=options)
+                chrome.get(url)
+            except exceptions.WebDriverException as e:
+                print(time.strftime("%Y/%m/%d %H:%M:%S",
+                                    time.localtime()), "Webdriver error:", e.msg)
+                chrome.close()
+                PhotoTaskBusyFlag = False
+                return
+            self.Login_maimai_net(chrome, user_tmp, pawd_tmp)
+            # 抓使用者
+            user = await client.fetch_user(user_id=int(userDcId))
+            print(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()),
+                  "正在確認"+user.display_name+"的新照片")
+            photoFiles = GetNewPhotos(chrome, userDcId)
+            if len(photoFiles) != 0:
+                # 機器人傳送圖片
+                for image in photoFiles:
+                    await user.send(file=discord.File(image))
+                    print(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()),
+                          '正在傳送新照片給' + user.display_name + ':'+image)
+                    # time.sleep(1)
+                    # 0402 wait test
+                    await asyncio.sleep(1)
+            print(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()), '確認結束')
+            chrome.close()
+        PhotoTaskBusyFlag = False
+
+    @GetNewPhotosTask.before_loop
+    async def BeforeTask(self):
+        global PhotoTaskBusyFlag
+        PhotoTaskBusyFlag = False
+        print('結束循環')
+
+
+global photosTask
+photosTask = None
 
 
 @ client.event
 # 當機器人完成啟動時
 async def on_ready():
-    print('目前登入身份：', client.user)
+    global PhotoTaskBusyFlag
+    print(time.strftime("%Y/%m/%d %H:%M:%S",
+          time.localtime()), '目前登入身份：', client.user)
     # user = await client.fetch_user(user_id=378159148862275584)
     # await user.send('start bot')
-    GetNewPhotosTask.start()
+    PhotoTaskBusyFlag = False
+    # if not GetNewPhotosTask.is_running():
+    #     GetNewPhotosTask.start()
+    global photosTask
+    if photosTask != None:
+        print('Del old task')
+        del(photosTask)
+    photosTask = PhotosTask()
+
+    # await asyncio.sleep(5)
+# task = [
+#     asyncio.ensure_future(GetNewPhotosTask.start())
+# ]
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(on_ready())
 
 
-@ client.event
+@client.event
+async def on_disconnect():
+    print(time.strftime("%Y/%m/%d %H:%M:%S",
+          time.localtime()), '機器人離線')
+
+
+@client.event
 # 當有訊息時
 async def on_message(message):
     # 排除自己的訊息，避免陷入無限循環
@@ -342,7 +437,6 @@ async def on_message(message):
                     return
                 elif err == 0:
                     await message.author.send('已設定自動存圖，最慢10分鐘後你會看到圖片uwu')
-
                     return
         elif tmp[1] == 'userid':
             await message.author.send(message.author)
@@ -352,55 +446,13 @@ async def on_message(message):
     elif message.content.startswith('ping'):
         await message.channel.send("pong")
 
-global PhotoTaskBusyFlag
-PhotoTaskBusyFlag = False
-
-
-@ tasks.loop(minutes=10)
-async def GetNewPhotosTask():
-    global PhotoTaskBusyFlag
-    if datetime.datetime.now().time() > NOT_IN_SERVICE_START_TIME.time() and datetime.datetime.now().time() < NOT_IN_SERVICE_END_TIME.time():
-        print('maimai net維修')
-        return
-    if PhotoTaskBusyFlag:
-        return
-    if GetUserList() != 0:
-        print("GetUserList error")
-        return
-    for users in userCollection:
-
-        user_tmp = users.get('valuse').get('username')
-        pawd_tmp = users.get('valuse').get('password')
-        userDcId = users.get('discordID')
-        PhotoTaskBusyFlag = True
-
-        chrome = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
-        chrome.get(url)
-        Login_maimai_net(chrome, user_tmp, pawd_tmp)
-
-        # 抓使用者
-        user = await client.fetch_user(user_id=int(userDcId))
-        print("正在確認"+user.display_name+"的新照片")
-        photoFiles = GetNewPhotos(chrome, userDcId)
-
-        if len(photoFiles) != 0:
-
-            # 機器人傳送圖片
-            for image in photoFiles:
-                await user.send(file=discord.File(image))
-                print('正在傳送新照片給' + user.display_name + ':'+image)
-                time.sleep(1)
-        print('確認結束')
-        chrome.close()
-
-    PhotoTaskBusyFlag = False
+logger = logging.getLogger('discord')
+logger.setLevel(logging.WARNING)
+handler = logging.FileHandler(
+    filename='logs/discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 client.run(DISCORD_BOT_TOKEN)
-
-
-# def main():
-#     chrome = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
-#     chrome.get(url)
-#     Login_maimai_net(chrome, usernameCollection[0], passwordCollection[0])
-#     GetNewPhotos(chrome, usernameCollection[0])
--- t.py
+++ t.py
@@ -1,14 +1,24 @@
-import json
-import ast
-author = "12342142131"
-username = '13"21321'
-passwd = '3213213'
-username = username.replace("'", "\\'").replace('"', '\\"')
-js = '{\
-        "discordID": "'+author + '",\
-        "valuse": {\
-            "username": "'+username+'",\
-            "password": "'+passwd+'"\
-    }}'
-print(js)
-print(json.loads((js)))
+from discord.ext import tasks
+import asyncio
+
+
+class Cache:
+    def __init__(self):
+        self.cache_update_loop.start()
+        print('__init__')
+
+    @tasks.loop(seconds=1)
+    async def cache_update_loop(self):
+        print(1)
+
+    @cache_update_loop.before_loop
+    async def before_cache_update_loop(self):
+        print('before loop')
+
+
+async def main():
+    cache = Cache()
+    await asyncio.sleep(5)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
